da1996f92bde19752bee3c43cef8ac5f102f3a05
ahmzz <ahmedminhas247@outlook.com>
2023-09-04 23:02:06 +0500

    Tailwind config setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,9 +16,19 @@ export default {
         }
       ),
       fontFamily:{
+        dmsans:["DM Sans","sans-serif"],
+        montserrat:["Montserrat","sans-serif"]
+      },
+      content:{
         
-      }
+      },
+      
     },
+    screens:{
+      xs:"480px",
+      sm:"768px",
+      md:"1060px"
+    }
   },
   plugins: [],
 };
